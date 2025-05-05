3ac6b8aadcc882a551c1f7a20b0451bbe91676aa
Ivan Masmitjà <imasmitja@gmail.com>
2025-05-05 17:25:59 +0200

    Add email_validator package

--- lib/infrastructure/inputs/email_input.dart
+++ lib/infrastructure/inputs/email_input.dart
@@ -1,11 +1,11 @@
 import 'package:formz/formz.dart';
+import 'package:email_validator/email_validator.dart';
 
 // Define input validation errors
 enum EmailInputError { empty, invalidFormat }
 
 // Extend FormzInput and provide the input type and error type.
 class EmailInput extends FormzInput<String, EmailInputError> {
-  static RegExp emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
   // Call super.pure to represent an unmodified form input.
   const EmailInput.pure() : super.pure('');
 
@@ -17,7 +17,7 @@ class EmailInput extends FormzInput<String, EmailInputError> {
   EmailInputError? validator(String value) {
     if (value.isEmpty) return EmailInputError.empty;
 
-    if (!emailRegex.hasMatch(value)) {
+    if (!EmailValidator.validate(value)) {
       return EmailInputError.invalidFormat;
     }
     return null; // Input is valid
